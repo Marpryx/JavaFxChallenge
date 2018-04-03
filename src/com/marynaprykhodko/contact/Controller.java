a3b0d23dbbf8c8d69ebbe76304f8c88e04b54c10
Marpryx <pritismar@hotmail.com>
2018-04-03 16:18:51 -0400

    Initial commit

---
+++ src/com/marynaprykhodko/contact/Controller.java
@@ -0,0 +1,11 @@
+package com.marynaprykhodko.contact;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    public void showAddContact(){
+
+    }
+}
---
+++ src/com/marynaprykhodko/contact/NewContactController.java
@@ -0,0 +1,4 @@
+package com.marynaprykhodko.contact;
+
+public class NewContactController {
+}
